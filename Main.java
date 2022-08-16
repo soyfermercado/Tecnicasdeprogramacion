1026a950d1b20b7097ad4a989f8663e8e70bc608
soyfermercado <111364313+soyfermercado@users.noreply.github.com>
2022-08-16 12:35:03 -0300

    Add files via upload
    Primera Clase (archivo con código final)

---
+++ Main.java
@@ -0,0 +1,63 @@
+/*
+    Curso: Técnicas de programación 120 hs
+    Días: Martes presencial 9:00 a 13 hs
+          Jueves Virtual    9:00 a 13 hs
+          
+    Profesor: Carlos Rios     c.rios@bue.edu.ar
+    
+    aula virtual: meet.google.con/apo-fedn-cwn
+    
+    materiales:
+                github: https://github.com/dev-java-lts/tecnicas-cmd
+                moodle: https://aulasvirtuales.bue.edu.ar/
+                
+    materiales extra:
+                https://www.tutorialesprogramacionya.com/
+                https://open-bootcamp.com/
+                
+    software:   JDK (Java Development Kit - Kit de Desarrollo Java) 
+                Oracle JDK 17 dev-java-lts (Long Term Support 8 años)
+                https://www.oracle.com/java/technologies/downloads/
+                
+                Opción para 32 bits 
+                https://adoptopenjdk.net/releases.html
+                
+                IDE (Integrated Development Enviroment - Entorno de Desarrollo Integrado)
+                Visual Studio Code https://code.visualstudio.com/
+                
+                Nota: para verificar la instalación de java en consola en consola ejecuto java -version
+                
+
+
+*/
+// linea de comentarios
+/*
+    Bloque 
+    de 
+    comentarios
+*/
+
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        //Escribimos adentro del método main
+        
+        System.out.println("Hello world!!!"); //Imprime en consola
+        //Lenguaje en casa sensitive - Lenguaje sensible a mayúsculas y minúsculas 
+        // ; es el terminador de sentencias
+        // F8 para ejecutar en www.online-ide.com
+        // F5 para ejectuar en vcode.
+        
+        System.out.println("Centro de Formación Profesional N35");
+        System.out.println("Centro Metropolitano de Diseño!");
+        
+        
+        
+        
+        
+        
+        
+    }//end main method
+}// end main class
